817e880e19d66b385a22a6794c2d273abf1c3b0c
Justin Cole <justin.cole@digitas.com>
2023-07-27 11:03:49 +1200

    initial commit

---
+++ app/_components/Post.tsx
@@ -0,0 +1,47 @@
+// ./nextjs-app/app/_components/Post.tsx
+
+"use client";
+
+import Image from "next/image";
+import imageUrlBuilder from "@sanity/image-url";
+import { SanityDocument } from "@sanity/client";
+import { PortableText } from "@portabletext/react";
+import { client } from "@/sanity/lib/client";
+
+const builder = imageUrlBuilder(client);
+
+export default function Post({ post }: { post: SanityDocument }) {
+  return (
+    <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
+      <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+        <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+          <div className="lg:pr-4">
+            <div className="lg:max-w-lg">
+              <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                {post.title}
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+          {post?.mainImage ? (
+            <Image
+              className="float-left m-0 w-full lg:w-1/2 mr-4 rounded-lg"
+              src={builder.image(post.mainImage).width(1024).height(768).url()}
+              width={1024}
+              height={768}
+              alt={post?.mainImage?.alt}
+            />
+          ) : null}
+        </div>
+        <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+          <div className="lg:pr-4">
+            <div className="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg prose lg:prose-xl">
+              {post?.body ? <PortableText value={post.body} /> : null}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
